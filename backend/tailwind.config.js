171f363e79325387005c5b3f4956eb0bb594916c
jhoan2317 <jhoanhrndz22@gmail.com>
2025-03-15 20:50:07 -0500

    css

--- backend/tailwind.config.js
+++ backend/tailwind.config.js
@@ -1,10 +1,22 @@
 import preset from './vendor/filament/filament/tailwind.config.preset'
 
+/** @type {import('tailwindcss').Config} */
 export default {
     presets: [preset],
     content: [
         './app/Filament/**/*.php',
+        './resources/views/**/*.blade.php',
         './resources/views/filament/**/*.blade.php',
         './vendor/filament/**/*.blade.php',
+        './resources/js/**/*.js',
+        './resources/css/**/*.css',
+    ],
+    darkMode: 'class',
+    theme: {
+        extend: {},
+    },
+    plugins: [
+        require('@tailwindcss/forms'),
+        require('@tailwindcss/typography'),
     ],
 } 
